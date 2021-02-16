333fc11d1325d401024cbbd09cd38ed664073ff6
Demanxier <demanxier.rodrigues@gmail.com>
2021-02-16 12:41:02 -0300

    App Concluido

---
+++ Interfaces/IRepositorio.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DIO.Series
+    interface IRepositorio<T>
+    {
+        List<T> Listas();
+        T RetornaPorId(int id);
+        void Insere(T entidade);
+        void Exclui(int id);
+        void Atualiza(int id, T entidade);
+        int ProximoId();
+    }
+}
